7a4d6058ab8bf9fa8b5d9463d1ffd5c28a2bcf5e
Mayur Soni <knox19830@gmail.com>
2025-01-22 18:29:22 +0530

    #2

---
+++ client/src/App.jsx
@@ -0,0 +1,20 @@
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { Button } from "./components/ui/button";
+import Auth from "./pages/auth/Auth.1";
+import Chat from "./pages/chat/Chat";
+import Profile from "./pages/profile/Profile";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="*" element={<Navigate to="/auth" />} />
+        <Route path="/auth" element={<Auth />} />
+        <Route path="/chat" element={<Chat />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
