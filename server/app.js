ea7e3be9321d6be8ebaef227a2b2696d30594095
Mayur <mayursoni2705@gmail.com>
2025-01-24 17:06:31 +0530

    #4

---
+++ server/app.js
@@ -0,0 +1,32 @@
+import expess from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+import mongoose from "mongoose";
+
+dotenv.config();
+
+const app = expess();
+const port = process.env.PORT || 3000;
+const databaseUrl = process.env.DATABASE_URL;
+
+app.use(cors({
+    origin:[process.env.ORIGIN],
+    methods:["GET", "POST", "PUT", "PATCH", "DELETE"],
+    credentials:true
+}))
+
+app.use(cookieParser())
+app.use(expess.json())
+
+app.listen(port, () => {
+    console.log(`The Server is running on ${port}`);
+});
+
+mongoose
+    .connect(databaseUrl)
+    .then(() => {
+        console.log("Database connected successfully");
+    })
+    .catch((err) => console.log(err.message));
