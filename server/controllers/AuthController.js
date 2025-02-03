e46b4e28693a4b3cfe934a941b089e7229987dbb
mayursoni5 <mayursoni2705@gmail.com>
2025-02-03 23:48:02 +0530

    #9

--- server/controllers/AuthController.js
+++ server/controllers/AuthController.js
@@ -98,3 +98,32 @@ export const getUserInfo = async (req, res, next) => {
     return res.status(500).send("Internal Server Error");
   }
 };
+
+export const updateProfile = async (req, res, next) => {
+  try {
+    const { userId } = req;
+    const { firstName, lastName, color } = req.body;
+    if (!firstName || !lastName) {
+      return res.status(400).send("firstname lastname or color is required.");
+    }
+
+    const userData = await User.findByIdAndUpdate(
+      userId,
+      { firstName, lastName, color, profileSetup: true },
+      { new: true, runValidators: true }
+    );
+
+    return res.status(200).json({
+      id: userData.id,
+      email: userData.email,
+      profileSetup: userData.profileSetup,
+      firstName: userData.firstName,
+      lastName: userData.lastName,
+      image: userData.image,
+      color: userData.color,
+    });
+  } catch (error) {
+    console.log({ error });
+    return res.status(500).send("Internal Server Error");
+  }
+};
