3ab05af1ef59cdc263b808c0f6f16792a9b7a7d1
mayursoni5 <mayursoni2705@gmail.com>
2025-02-10 00:03:48 +0530

    12

--- client/src/pages/chat/components/chat-container/components/message-container/MessageContainer.jsx
+++ client/src/pages/chat/components/chat-container/components/message-container/MessageContainer.jsx
@@ -1,5 +1,7 @@
 function MessageContainer() {
-  return <div>MessageContainer</div>;
+  return (
+    <div className="flex-1 overflow-y-auto scrollbar-hidden p-4 px-8 md:w-[65vw] lg:w-[70vw] xl:w-[80vw] w-full"></div>
+  );
 }
 
 export default MessageContainer;
